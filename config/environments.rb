167238827b1205e1dd231cb13b08ca27f8a5fd5c
Ulukbek Nusubaliev <ulukbeknusubaliev@Ulukbeks-MacBook-Air.local>
2019-05-30 16:09:14 -0500

    fixed and error

--- config/environments.rb
+++ config/environments.rb
@@ -3,7 +3,7 @@ configure :production, :development do
 	db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/sale_products')
 
 	ActiveRecord::Base.establish_connection(
-		:adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme
+		:adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
 		:host => db.host,
 		:username => db.user,
 		:password => db.password,
